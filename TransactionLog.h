5b1cf2af7ed8c4abb92bdbf4c16bddce09c60ad0
linnsheng <135851909+CLinnSheng@users.noreply.github.com>
2024-09-09 00:50:49 +0800

    Update TransactionLog.h

--- TransactionLog.h
+++ TransactionLog.h
@@ -1,26 +1,46 @@
-#include "TransactionLog.h"
-void TransactionLog::addTransaction(const std::string& description)
-{
-	Transactions_.emplace_back(Transaction{ std::chrono::system_clock::now(), description });
-}
+#pragma once
+#include <vector>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
 
-void TransactionLog::clear()
+class TransactionLog
 {
-	Transactions_.clear();
-}
-
-std::string TransactionLog::getFormattedLog() const
-{
-	std::stringstream ss;
-	ss << "Transaction Log:\n";
-
-	for (const auto& transaction : Transactions_)
+public:
+	struct Transaction
 	{
-		auto time = std::chrono::system_clock::to_time_t(transaction.timestamp);
-		std::tm tm_;
-		localtime_s(&tm_, &time);
-		ss << std::put_time(&tm_, "%d/%m/%Y %H:%M:%S") << " - " << transaction.description << std::endl;
+		std::chrono::system_clock::time_point timestamp;
+		std::string description;
+	};
+
+	void addTransaction(const std::string& description)
+	{
+		Transactions_.emplace_back(Transaction{ std::chrono::system_clock::now(), description });
+	}	
+	
+	void clear()
+	{
+		Transactions_.clear();
 	}
 
-	return ss.str();
-}
+	std::string getFormattedLog() const
+	{
+		std::stringstream ss;
+		ss << "Transaction Log:\n";
+
+		for (const auto& transaction : Transactions_)
+		{
+			auto time = std::chrono::system_clock::to_time_t(transaction.timestamp);
+			std::tm tm_;
+			localtime_s(&tm_, &time);
+			ss << std::put_time(&tm_, "%d/%m/%Y %H:%M:%S") << " - " << transaction.description << std::endl;
+		}
+
+		return ss.str();
+	}
+
+private:
+	std::vector<Transaction> Transactions_;
+};
+
